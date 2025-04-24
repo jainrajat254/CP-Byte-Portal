af2b427fb9707b6d8242aa1ff9f9343fb99abfb6
ansh <anshsingh1119@gmail.com>
2025-04-24 18:33:30 +0530

    test commit

--- app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/LoginScreen.kt
@@ -1,10 +1,12 @@
 package com.example.cpbyte_portal.presentation.ui.screens
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 
 
 @Composable
 fun LoginScreen() {
+    TextField(value = "safkjhad", onValueChange = {})
     Text(text = "hello world")
 }

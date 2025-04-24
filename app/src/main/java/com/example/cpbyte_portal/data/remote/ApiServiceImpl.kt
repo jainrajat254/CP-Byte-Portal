a501d3903069e2eb6622394eb9dda07e15df7ee9
Rajat Jain <254jainrajat@gmail.com>
2025-04-25 00:40:26 +0530

    added coordinator's endpoints

--- app/src/main/java/com/example/cpbyte_portal/data/remote/ApiServiceImpl.kt
+++ app/src/main/java/com/example/cpbyte_portal/data/remote/ApiServiceImpl.kt
@@ -1,23 +1,78 @@
 package com.example.cpbyte_portal.data.remote
 
-import com.example.cpbyte_portal.domain.service.ApiService
+import com.example.cpbyte_portal.domain.model.DomainUsersResponse
+import com.example.cpbyte_portal.domain.model.FetchAttendanceResponse
 import com.example.cpbyte_portal.domain.model.LoginRequest
 import com.example.cpbyte_portal.domain.model.LoginResponse
+import com.example.cpbyte_portal.domain.model.LogoutResponse
+import com.example.cpbyte_portal.domain.model.MarkAttendance
+import com.example.cpbyte_portal.domain.model.MarkAttendanceResponse
+import com.example.cpbyte_portal.domain.service.ApiService
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 
 
-class ApiServiceImpl(private val client: HttpClient): ApiService {
-    private val BASE_URL="https://cpbyte-student-portal.onrender.com/api"
+class ApiServiceImpl(private val client: HttpClient) : ApiService {
+    private val BASE_URL = "https://cpbyte-student-portal.onrender.com/api"
 
-    override suspend fun login(loginRequest: LoginRequest):LoginResponse{
-        return client.post("$BASE_URL/v1/auth/login"){
+    //Login Impl.
+    override suspend fun login(loginRequest: LoginRequest): LoginResponse {
+        return client.post("$BASE_URL/v1/auth/login") {
             contentType(ContentType.Application.Json)
             setBody(loginRequest)
         }.body()
     }
+
+    //Logout Impl.
+    override suspend fun logout(token: String): LogoutResponse {
+        return client.get("$BASE_URL/v1/auth/logout") {
+            headers {
+                append("Authorization", "Bearer $token")
+            }
+        }.body()
+    }
+
+    //fetch All Attendance Impl.
+    override suspend fun fetchAllAttendance(token: String): FetchAttendanceResponse {
+        return client.get("$BASE_URL/v1/coordinator/attendance") {
+            headers {
+                append("Authorization", "Bearer $token")
+            }
+        }.body()
+    }
+
+
+    //Mark Attendance Impl.
+    override suspend fun markAttendance(
+        token: String,
+        markAttendance: MarkAttendance,
+    ): MarkAttendanceResponse {
+        return client.post("$BASE_URL/v1/coordinator/markAttendance") {
+            contentType(ContentType.Application.Json)
+            setBody(markAttendance)
+            headers {
+                append("Authorization", "Bearer $token")
+            }
+        }.body()
+    }
+
+    //fetch members of domain
+    override suspend fun membersOfDomain(token: String, domain: String): DomainUsersResponse {
+        return client.get("$BASE_URL/v1/coordinator/memberOfDomain") {
+            url {
+                parameters.append("domain", domain)
+            }
+            headers {
+                append("Authorization", "Bearer $token")
+            }
+        }.body()
+    }
+
+
 }
--- app/src/main/java/com/example/cpbyte_portal/di/NetworkModule.kt
+++ app/src/main/java/com/example/cpbyte_portal/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.example.cpbyte_portal.di
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
@@ -17,6 +18,11 @@ val networkModule = module{
             install(HttpTimeout){
                 requestTimeoutMillis=15_000
             }
+//            install(DefaultRequest) {
+//                TokenProvider.token?.let {
+//                    headers.append("Authorization", "Bearer $it")
+//                }
+//            }
         }
     }
 }

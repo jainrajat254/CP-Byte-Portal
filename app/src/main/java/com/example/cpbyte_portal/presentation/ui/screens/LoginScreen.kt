3b1d109dad46bbaa9d8f2bf418d0aecb79e8c1cd
Pulkit Verma <pulkitverma@Pulkits-MacBook-Air.local>
2025-04-25 21:16:19 +0530

    checking

--- app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/LoginScreen.kt
@@ -3,8 +3,9 @@ package com.example.cpbyte_portal.presentation.ui.screens
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-
+@Preview(showBackground = true)
 @Composable
 fun LoginScreen() {
     Text(text = "lsidfjlas")

7e08dbfc0c03a57ba74ff1c91ab1acf4ae3a8f82
ansh <anshsingh1119@gmail.com>
2025-04-26 14:25:56 +0530

    login functionality added on screen

--- app/src/main/java/com/example/cpbyte_portal/di/AppModule.kt
+++ app/src/main/java/com/example/cpbyte_portal/di/AppModule.kt
@@ -4,9 +4,13 @@ import com.example.cpbyte_portal.domain.service.ApiService
 import com.example.cpbyte_portal.data.remote.ApiServiceImpl
 import com.example.cpbyte_portal.data.repository.LoginRepositoryImpl
 import com.example.cpbyte_portal.domain.repository.LoginRepository
+import com.example.cpbyte_portal.presentation.viewmodel.AuthViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.scope.get
 import org.koin.dsl.module
 
 val appModule = module {
     single<ApiService> { ApiServiceImpl(get()) }
     single<LoginRepository> { LoginRepositoryImpl(get()) }
+    viewModel { AuthViewModel(get()) }
 }

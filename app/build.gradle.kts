73bfb1c5bdcef70256f671151d834cb0e763af36
Rajat Jain <254jainrajat@gmail.com>
2025-04-27 00:48:50 +0530

    Added EventViewModel, made changes in AuthViewModel and made the spalsh screen(will later change the image with XML)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,8 @@ dependencies {
     implementation ("io.ktor:ktor-client-core:3.1.2") // for base functionality
     implementation("io.ktor:ktor-client-auth:3.1.2")
 
-
+    //Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     //navigation
     implementation("androidx.navigation:navigation-compose:2.8.9")
@@ -98,12 +99,7 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.1.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
 
-
     //serialization
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-
-    implementation ("io.ktor:ktor-client-android:2.3.4") // for Android client
-    implementation ("io.ktor:ktor-client-logging:2.3.4") // for logging plugin
-    implementation ("io.ktor:ktor-client-core:2.3.4") // for base functionality
 
 }
--- app/src/main/java/com/example/cpbyte_portal/presentation/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/cpbyte_portal/presentation/viewmodel/AuthViewModel.kt
@@ -4,29 +4,44 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cpbyte_portal.domain.model.LoginRequest
 import com.example.cpbyte_portal.domain.model.LoginResponse
+import com.example.cpbyte_portal.domain.model.LogoutResponse
 import com.example.cpbyte_portal.domain.repository.AuthRepository
 import com.example.cpbyte_portal.util.ResultState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class AuthViewModel(private val authRepository: AuthRepository): ViewModel() {
+class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
 
 
     private val _loginState = MutableStateFlow<ResultState<LoginResponse>>(ResultState.Idle)
     val loginState: StateFlow<ResultState<LoginResponse>> = _loginState
 
-    fun loginUser(email: String, password: String) {
+    private val _logoutState = MutableStateFlow<ResultState<LogoutResponse>>(ResultState.Idle)
+    val logoutState: StateFlow<ResultState<LogoutResponse>> = _logoutState
+
+    fun loginUser(libraryId: String, password: String) {
         _loginState.value = ResultState.Loading
         viewModelScope.launch {
             try {
-                val loginRequest = LoginRequest(email, password)
+                val loginRequest = LoginRequest(library_id = libraryId, password = password)
                 val loginResponse: LoginResponse = authRepository.login(loginRequest)
                 _loginState.value = ResultState.Success(loginResponse)
             } catch (e: Exception) {
-                _loginState.value=ResultState.Failure(e)
+                _loginState.value = ResultState.Failure(e)
             }
         }
     }
 
+    fun logoutUser() {
+        _logoutState.value = ResultState.Loading
+        viewModelScope.launch {
+            try {
+                val logoutResponse: LogoutResponse = authRepository.logout()
+                _logoutState.value = ResultState.Success(logoutResponse)
+            } catch (e: Exception) {
+                _logoutState.value = ResultState.Failure(e)
+            }
+        }
+    }
 }

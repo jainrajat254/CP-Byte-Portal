5a5cbbcff65efa5f3fdcdacce92d489d7f8f485a
sikap <ksiya1306@gmail.com>
2025-04-28 23:58:50 +0530

    Improved the alignment

--- app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/components/CPByteButton.kt
+++ app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/components/CPByteButton.kt
@@ -12,31 +12,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun CPByteButton(
-    value: String,
+    value: String, // Text to display inside the button
     onClick: () -> Unit // no input, returns nothing
 ) {
     Button(
-        onClick = onClick,
+        onClick = onClick, // Handles button clicks
         colors = ButtonDefaults.buttonColors(
             contentColor = Color.White,
             containerColor = Color(0xFF5A55FF)
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color(0xFF121212)) //button with gradient background
-            .height(50.dp)
+            .background(Color(0xFF121212)) //Button with gradient background
+            .height(44.dp)
             .width(280.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = ButtonDefaults.buttonElevation() // button looks elevated
+        shape = RoundedCornerShape(10.dp),
+        elevation = ButtonDefaults.buttonElevation() // Adds a shadow, button looks elevated
     ) {
         Text(
             text = value,
-            fontSize = 20.sp
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
         )
     }
 

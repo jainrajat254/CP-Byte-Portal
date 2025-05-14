39eebea1c1ff2494c37083b992af94b4475ee87e
divyakumari7 <divyakumari352932@gmail.com>
2025-05-14 13:52:27 +0530

    manage tracker skills settings, remove project, coding platform scrrens added

--- app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/SkillsSettingsScreen.kt
+++ app/src/main/java/com/example/cpbyte_portal/presentation/ui/screens/SkillsSettingsScreen.kt
@@ -1,4 +1,4 @@
-package com.example.cpbyte_portal.presentation.ui.screens.components
+package com.example.cpbyte_portal.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
